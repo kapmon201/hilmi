1effd6dcf5e2a02953beabc55f90d2f3db5924f1
kapmon201 <103316938+kapmon201@users.noreply.github.com>
2022-09-24 16:58:57 +0700

    upload 013.php
    file dari komputer

---
+++ 013.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>016</title>
+</head>
+<body>
+
+<script>
+	console.info('ini console.info');
+	console.warn('ini warn');
+	console.error('ini error');	
+
+</script>
+
+
+</body>
+</html>
